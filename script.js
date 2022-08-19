48906ad9e19a2b6bfd97dd568c6f224112f107a4
b0hdnan <boplan1985@gmail.com>
2022-08-19 17:46:34 +0300

    game is ready

--- script.js
+++ script.js
@@ -34,7 +34,27 @@ var playCards = document.querySelectorAll("div.box");
 for (let i = 0; i < playCards.length; i++) {
 	playCards[i].setAttribute("id", i); // встановлюємо id для кожної картки
 	playCards[i].addEventListener("click", function () {
-	this.style.backgroundImage = `url(${shuffleArray[i].img})`;
+	this.style.backgroundImage = `url(${shuffleArray[i].img})`; // з'являється зображення
+	choosenCards.push(shuffleArray[this.id].name) // додаємо ім'я картки в масив вибраних карток
+  choosenCardsId.push(this.id)	// додаємо Id картки в масив вибраних карток Id
+
+	// якщо в масивах дві картки...
+  if (choosenCards.length === 2) {
+		setTimeout(function (){ // відстрочка виконання функції
+			if (choosenCards[0] === choosenCards[1]){
+				// приховуємо картки
+				playCards[choosenCardsId[0]].style.visibility = "hidden";
+				playCards[choosenCardsId[1]].style.visibility = "hidden";
+			}
+			else{
+				playCards[choosenCardsId[0]].style.backgroundImage = "none";
+				playCards[choosenCardsId[1]].style.backgroundImage = "none";
+			}
+			choosenCards = [];
+			choosenCardsId = [];
+		}, 500)
+	}
+
 	});
 }
 // подвоєний масив карток і підготовлений для гри масив shuffleArray
